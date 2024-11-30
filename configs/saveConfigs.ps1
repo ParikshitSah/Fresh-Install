8ad0949ed7ae93b6f77f927535db38aec20b244a
Parikshit Sah <parikshit.sah12@gmail.com>
2024-11-30 17:58:16 -0500

    FIX: wrong destination for keybindings.json

--- configs/saveConfigs.ps1
+++ configs/saveConfigs.ps1
@@ -1,13 +1,9 @@
 # Declare all destination file paths
-$vscodeExtensionsFile = "configs\VSCode\extensions.txt"
-$vscodeSettingsFile = "configs\VSCode"
-$flowLauncherFolder = "configs\FlowLauncher"
-$ohMyPoshConfigFile = "configs\OhMyPosh\config.json"
-
+. .\variables.ps1
 # Backup VSCode extensions and settings
 code --list-extensions > $vscodeExtensionsFile
-Copy-Item "$env:APPDATA\Code\User\settings.json" -Destination $vscodeSettingsFile
-Copy-Item "$env:APPDATA\Code\User\keybindings.json" -Destination $vscodeSettingsFile
+Copy-Item "$env:APPDATA\Code\User\settings.json" -Destination $vscodeSettingsFile -Force   
+Copy-Item "$env:APPDATA\Code\User\keybindings.json" -Destination $vscodeKeyBindingsFile -Force
 
 # Backup Oh My Posh configuration
 oh-my-posh config export > $ohMyPoshConfigFile
